2fc89df45cc3a506d03e0c1a5a4b602ba0cf9fc5
Aryan <aryanmaurya879@gmail.com>
2024-03-31 10:09:16 +0530

    added blue color to selected sidebar component

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Recordings
  = () => {
   return (
-    <div>Recordings
-        
-    </div>
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <h1 className='text-3xl font-bold'>
+        RECORDEINGS 
+      </h1>
+    </section>
   )
 }
 
