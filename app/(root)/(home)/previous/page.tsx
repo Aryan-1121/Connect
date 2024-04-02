2737274e4fc15a72de8f7ad9c54b068ecc033f5d
Aryan <aryanmaurya879@gmail.com>
2024-04-03 01:30:35 +0530

    added CallList component to our upcoming and previous meeting pages

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,12 +1,14 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Previous = () => {
   return (
-<section className='flex size-full flex-col gap-10 text-white'>
+    <section className='flex size-full flex-col gap-10 text-white'>
       <h1 className='text-3xl font-bold'>
-        PREVOOUS  
+        PREVIOUS
       </h1>
-    </section>  )
+      <CallList type='ended' />
+    </section>)
 }
 
 export default Previous
--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -5,7 +5,8 @@ const Recordings
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <h1 className='text-3xl font-bold'>
-        RECORDEINGS 
+        RECORDINGS
+ 
       </h1>
     </section>
   )
