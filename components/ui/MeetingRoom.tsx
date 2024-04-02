fe9f7cf779e885ea4fa698cc0de410cb3a3ecd57
Aryan <aryanmaurya879@gmail.com>
2024-04-02 10:07:59 +0530

    added dropdown for left,right & grid view

--- components/ui/MeetingRoom.tsx
+++ components/ui/MeetingRoom.tsx
@@ -2,6 +2,10 @@ import { cn } from '@/lib/utils';
 import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
 import { useState } from 'react';
 
+import {DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuLabel,DropdownMenuSeparator,DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
+import { LayoutList } from 'lucide-react';
+
+
 
 
 
@@ -18,13 +22,13 @@ const MeetingRoom = () => {
   //    now since we know the type so we can create a new functional component which will render a specific layout depending upon the current layout state
 
   const CallLayout = () => {
-    switch(layOut){
+    switch (layOut) {
       case 'grid':
         return <PaginatedGridLayout />
       case 'speaker-left':
-        return <SpeakerLayout participantsBarPosition = 'right'/>
+        return <SpeakerLayout participantsBarPosition='right' />
       default:
-        return <SpeakerLayout participantsBarPosition = 'left'/>
+        return <SpeakerLayout participantsBarPosition='left' />
 
     }
   }
@@ -46,9 +50,33 @@ const MeetingRoom = () => {
       </div>
       {/* video layout and call controls */}
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
-        <CallControls  />
+        <CallControls />
 
-        
+          {/*  dropdown to change layout coming from shadcn */}
+
+        <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {['Grid', 'Speaker-Left', 'Speaker-Right'].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem className='cursor-pointer'
+                  onClick={() =>
+                    setlayOut(item.toLowerCase() as CallLayoutType)
+                  }
+                >
+                  {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-dark-1" />
+              </div>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+
       </div>
     </section>
   );
