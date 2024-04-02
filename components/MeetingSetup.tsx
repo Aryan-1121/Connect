ee98a39560fe7527052e47d8553b5366216b8c19
Aryan <aryanmaurya879@gmail.com>
2024-04-02 08:45:18 +0530

    meeting setup/preview created before joining meeting

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+'use client'
 
-const MeetingSetup = () => {
+import { DeviceSettings, VideoPreview, useCall } from '@stream-io/video-react-sdk';
+import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button';
+
+const MeetingSetup = ({ setIsSetupComplete }: {setIsSetupComplete: (value:boolean)=>void }) => {
+
+  const [isMicCamToggled, setIsMicCamToggled] = useState(false);
+
+  
+
+  const call = useCall();
+
+  if(!call){
+    throw new Error('usecall must be used within StreamCall component');
+  }
+
+  //  creating a useEffect to monitor changes to isMicCamToggled
+  useEffect(() => {
+    if (isMicCamToggled) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+
+  }, [isMicCamToggled, call?.camera, call?.microphone]);
+
   return (
-    <div>MeetingSetup</div>
-  )
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-center text-2xl font-bold">Preview Setup</h1>
+      {/* VideoPreview comes from react-sdk to preview video  */}
+      <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggled}
+            onChange={(e) => setIsMicCamToggled(e.target.checked)}
+          />
+          Join with mic and camera off
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        className="rounded-md bg-green-500 px-4 py-2.5"
+        onClick={() => {
+          call.join();
+
+          setIsSetupComplete(true);
+        }}
+      >
+        Join meeting
+      </Button>
+    </div>)
 }
 
 export default MeetingSetup
